686261463b93a2d428e2412a235ecbbdc2fa651d
josephmabbinante-a11y <joseph.m.abbinante@gmail.com>
2026-03-08 04:17:38 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import './index.css';
-import FleetDashboard from './components/FleetDashboard.jsx';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+// ...import other pages/components as needed
+
+function PrivateRoute({ children }) {
+  const token = localStorage.getItem('accessToken');
+  return token ? children : <Navigate to="/login" />;
+}
 
 function App() {
-  return <FleetDashboard />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        {/* Add other routes here */}
+        <Route path="*" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
